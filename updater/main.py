eed2c2274453aa9bb53acde1ea034853ba7d6fa6
boba2fett <boba2002fett@gmail.com>
2020-02-20 14:47:35 +0100

    trys

---
+++ updater/main.py
@@ -0,0 +1,4 @@
+def main():
+    if check()
+        exec updater
+    
---
+++ updater/updater.py
@@ -0,0 +1,8 @@
+https://github.com/boba2fett/ShitCollection/raw/master/updater/main.py
+
+check
+return True
+
+update()
+    wget 
+    exec
